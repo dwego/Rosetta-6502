92e2b8a1fd437b19f4aebce7b20620c78e7cdbdf
dwego <97767711+dwego@users.noreply.github.com>
2024-01-15 02:53:23 -0300

    Update cpu6502.h

--- CPU/6502/cpu6502.h
+++ CPU/6502/cpu6502.h
@@ -26,14 +26,14 @@
 
 typedef struct StatusFlags
 {	
-	Byte C : 1;	//0: Carry Flag	
-	Byte Z : 1;	//1: Zero Flag
-	Byte I : 1; //2: Interrupt disable
-	Byte D : 1; //3: Decimal mode
-	Byte B : 1; //4: Break
-	Byte Unused : 1; //5: Unused
-	Byte V : 1; //6: Overflow
-	Byte N : 1; //7: Negative
+	Byte C : 1;	  // 0: Carry Flag	
+	Byte Z : 1;	  // 1: Zero Flag
+	Byte I : 1; 	  // 2: Interrupt disable
+	Byte D : 1; 	  // 3: Decimal mode
+	Byte B : 1; 	  // 4: Break
+	Byte Unused : 1;  // 5: Unused
+	Byte V : 1; 	  // 6: Overflow
+	Byte N : 1; 	  // 7: Negative
 } StatusFlags ;
 
 
