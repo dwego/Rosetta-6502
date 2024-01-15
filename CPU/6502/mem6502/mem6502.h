431beddd05fce7beaa3b3ec1085e89e8bc7a6877
dwego <97767711+dwego@users.noreply.github.com>
2024-01-15 02:32:36 -0300

    header mem6502

--- CPU/6502/mem6502/mem6502.h
+++ CPU/6502/mem6502/mem6502.h
@@ -3,12 +3,31 @@
 
 #include "../../../config/config.h"
 
+
+/* 
+   MEM6502 - 6502 Emulated Memory
+
+   This module provides an emulation of the memory system for the MOS Technology 6502 processor.
+
+   The MOS 6502 processor, utilized in various computers and game consoles of the 1970s and 1980s, interacts with
+   an addressable memory space. This module emulates a simplified version of this memory system, allowing the
+   6502 instructions to read from and write to memory.
+
+   The emulated memory is represented by a structure called MEM6502, containing an array of bytes to mimic RAM.
+   The maximum size of this memory is defined as MAX_MEM (65 Kilobytes).
+*/
+
+
+// Maximum memory size for the 6502 system.
 extern const DWord MAX_MEM;
 
+
+// Structure representing the memory for the 6502 system.
 typedef struct
 {
     Byte *Data; // Emulates RAM to allocate 65 Kilobytes for storing data.
 } MEM6502;
+
 
 // Initializes memory to 65 Kilobytes (64 * 1024 Bytes).
 void initializeMem6502(MEM6502 *memory);
