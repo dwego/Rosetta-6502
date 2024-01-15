0d89f8529edcacb222974f0dc9e3b85595a5e8d0
dwego <97767711+dwego@users.noreply.github.com>
2024-01-15 02:52:25 -0300

    header cpu6502

--- CPU/6502/cpu6502.h
+++ CPU/6502/cpu6502.h
@@ -3,6 +3,27 @@
 
 #include "../../config/config.h"
 
+/* 
+   CPU6502 - 6502 Emulated CPU
+
+   This file implements the CPU (Central Processing Unit) functionality for the MOS Technology 6502.
+
+   The MOS Technology 6502 is a widely used 8-bit microprocessor, and this implementation provides
+   functions for initializing the CPU, fetching bytes and words from memory, and stack manipulation.
+
+   This file implement instructions for functions defined in cpu6502.h
+
+   For more information about the instructions and addressing modes, refer to Instructions.MD
+*/ 
+
+
+/* 
+   StatusFlags - Status Flags for the CPU6502
+   
+   The structure defines individual status flags as bits in the Status Register (PS)
+*/ 
+
+
 typedef struct StatusFlags
 {	
 	Byte C : 1;	//0: Carry Flag	
@@ -15,6 +36,13 @@ typedef struct StatusFlags
 	Byte N : 1; //7: Negative
 } StatusFlags ;
 
+
+/* 
+   CPU6502 - CPU Structure for the MOS Technology 6502
+   
+   The structure represents the CPU state for the MOS Technology 6502.
+*/
+
 typedef struct {
     Byte A, X, Y;   // registers
     Byte SP;        // stack pointer
@@ -23,22 +51,40 @@ typedef struct {
 
     union // processor status
 	{
-		Byte PS;	
-		StatusFlags Flag;
+		Byte PS;	// Processor Status (PC)
+		StatusFlags Flag; // Flags 
 	};
 } CPU6502;
 
+
+// Reset:
+
+// This function resets the CPU state to its initial values.
 void resetCPU(Word ResetVector, CPU6502 *cpu, MEM6502 *memory);
 
+
+// Fetch Data:
+
+// This function fetches a byte of data from the memory using the program counter (PC).
 Byte FetchByte(Word *Cycles, const MEM6502* memory, CPU6502 *cpu);
+
+// This function fetches a 16-bit word of data from the memory using the program counter (PC).
 Word FetchWord(Word *Cycles, const MEM6502* memory, CPU6502 *cpu);
 
+
+// This function converts the Stack Pointer (SP) value to a memory address.
 Word SPToAddress(CPU6502 *cpu);
 
-void PushWordToStack(Word *Cycles, MEM6502 *memory, Word Value, CPU6502 *cpu);
-void PushPCToStack(Word *Cycles, MEM6502 *memory, CPU6502 *cpu);
-Word PopWordFromStack(Word *Cycles, MEM6502 *memory, CPU6502 *cpu)
 
-void execute(Word *Cycles, MEM6502 *memory, CPU6502 *cpu);
+//	Stacks: 
+
+// This function pushes a 16-bit word onto the stack.
+void PushWordToStack(Word *Cycles, MEM6502 *memory, Word Value, CPU6502 *cpu);
+
+// This function pushes the Program Counter (PC) onto the stack.
+void PushPCToStack(Word *Cycles, MEM6502 *memory, CPU6502 *cpu);
+
+// This function pops a 16-bit word from the stack.
+Word PopWordFromStack(Word *Cycles, MEM6502 *memory, CPU6502 *cpu)
 
 #endif // CPU6502_H
