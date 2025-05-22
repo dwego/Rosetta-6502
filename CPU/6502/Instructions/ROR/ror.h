a0b6d0c4e85f99e49c55f922d3dab8b946f16356
dwego <97767711+dwego@users.noreply.github.com>
2025-05-22 03:05:29 -0300

    Fixed ROR instruction code

--- CPU/6502/Instructions/ROR/ror.h
+++ CPU/6502/Instructions/ROR/ror.h
@@ -6,108 +6,130 @@
 #include "mem6502.h"
 
 /*
-   This is a header file for the ROL (Load Accumulator) instruction for MOS Technology 6502.
-   ROL works by moving a value into the Accumulator register (A).
+   This is a header file for the ROR (Load Accumulator) instruction for MOS Technology 6502.
+   ROR works by moving a value into the Accumulator register (A).
    For more information about the instructions, refer to Instructions.MD
 */
 
 /*
-   ROL (Load Accumulator) instruction supports various ROLressing modes in the 6502 architecture.
+   ROR (Load Accumulator) instruction supports various RORressing modes in the 6502 architecture.
    The different modes provide flexibility in specifying the source of the data to be loaded into the Accumulator (A).
 */
 
 
 /*
    This function sets the Flags for the Status register
-   to identify what happened during the ROL instruction.
+   to identify what happened during the ROR instruction.
 */
 
 
-void ROLSetStatus(Byte Value, CPU6502 *cpu) {
-    cpu->Flag.Z = (cpu->A == 0);
-    cpu->Flag.C = (Value & 0x80) ? 1 : 0;
-    cpu->Flag.N = (cpu->A & 0x80) > 0;
+void RORSetStatus(Byte originalValue, Byte result, CPU6502 *cpu) 
+{
+    cpu->Flag.C = (originalValue & 0x01) ? 1 : 0;
+    cpu->Flag.Z = (result == 0);
+    cpu->Flag.N = (result & 0x80) != 0;
 }
 
 
 /*
-   ROL_IM - Load Accumulator with Immediate value.
+   ROR_IM - Load Accumulator with Immediate value.
    This function fetches a byte from memory and loads it into the Accumulator (A).
-   It then sets the status flags using ROLSetStatus.
+   It then sets the status flags using RORSetStatus.
 */
 
 
-void ROL_IM(Word *Cycles, MEM6502 *memory, CPU6502 *cpu) {
-    Byte Value = FetchByte(Cycles, memory, cpu);
-    ROLSetStatus(Value, cpu);
-    cpu->A = (Value >> 1) | (cpu->Flag.C << 7);
+void ROR_ACC(Word *Cycles, CPU6502 *cpu) 
+{
+    Byte original = cpu->A;
+    Byte oldCarry = cpu->Flag.C;
+
+    Byte result = (original >> 1) | (oldCarry << 7);
+    cpu->A = result;
+    (*Cycles)--;
+
+    RORSetStatus(original, result, cpu);
 }
-
-
 /*
-   ROL_ZP - Load Accumulator from Zero Page.
-   This function fetches a byte representing a zero-page ROLress from memory, reads the
-   value at that ROLress, and loads it into the Accumulator (A). It then sets the status flags.
+   ROR_ZP - Load Accumulator from Zero Page.
+   This function fetches a byte representing a zero-page RORress from memory, reads the
+   value at that RORress, and loads it into the Accumulator (A). It then sets the status flags.
 */
 
 
-void ROL_ZP(Word *Cycles, MEM6502 *memory, CPU6502 *cpu) {
-    Byte ZeroPageROLr = FetchByte(Cycles, memory, cpu);
+void ROR_ZP(Word *Cycles, MEM6502 *memory, CPU6502 *cpu) 
+{
+    Byte addr = FetchByte(Cycles, memory, cpu);
 
-    Byte Value = ReadByte(Cycles, ZeroPageROLr, memory);
-    ROLSetStatus(Value, cpu);
-    cpu->A = (Value >> 1) | (cpu->Flag.C << 7);
+    Byte original = ReadByte(Cycles, addr, memory);
+    Byte oldCarry = cpu->Flag.C;
+
+    Byte result = (original >> 1) | (oldCarry << 7);
+    WriteByte(Cycles, result, memory, addr);
+
+    RORSetStatus(original, result, cpu);
 }
 
-
 /*
-   ROL_ZPX - Load Accumulator from Zero Page with X Offset.
-   Similar to ROL_ZP, but ROLs the X register value to the zero-page ROLress before reading
+   ROR_ZPX - Load Accumulator from Zero Page with X Offset.
+   Similar to ROR_ZP, but RORs the X register value to the zero-page RORress before reading
    the value from memory. It adjusts the cycle count accordingly and sets the status flags.
 */
 
 
-void ROL_ZPX(Word *Cycles, MEM6502 *memory, CPU6502 *cpu) {
-    Byte ZeroPageROLr = FetchByte(Cycles, memory, cpu);
-    ZeroPageROLr += cpu->X;
+void ROR_ZPX(Word *Cycles, MEM6502 *memory, CPU6502 *cpu) 
+{
+    Byte addr = FetchByte(Cycles, memory, cpu);
+    addr += cpu->X;
     (*Cycles)--;
 
-    Byte Value = ReadByte(Cycles, ZeroPageROLr, memory);
-    ROLSetStatus(Value, cpu);
-    cpu->A = (Value >> 1) | (cpu->Flag.C << 7);
+    Byte original = ReadByte(Cycles, addr, memory);
+    Byte oldCarry = cpu->Flag.C;
+
+    Byte result = (original >> 1) | (oldCarry << 7);
+    WriteByte(Cycles, result, memory, addr);
+
+    RORSetStatus(original, result, cpu);
 }
 
-
 /*
-   ROL_ABS - Load Accumulator from Absolute ROLress.
-   This function fetches a two-byte absolute ROLress from memory, reads the value at that ROLress,
+   ROR_ABS - Load Accumulator from Absolute RORress.
+   This function fetches a two-byte absolute RORress from memory, reads the value at that RORress,
    and loads it into the Accumulator (A). It then sets the status flags.
 */
 
 
-void ROL_ABS(Word *Cycles, MEM6502 *memory, CPU6502 *cpu) {
-    Word Absolute = FetchWord(Cycles, memory, cpu);
+void ROR_ABS(Word *Cycles, MEM6502 *memory, CPU6502 *cpu) 
+{
+    Word addr = FetchWord(Cycles, memory, cpu);
 
-    Byte Value = ReadByte(Cycles, Absolute, memory);
-    ROLSetStatus(Value, cpu);
-    cpu->A = (Value >> 1) | (cpu->Flag.C << 7);
+    Byte original = ReadByte(Cycles, addr, memory);
+    Byte oldCarry = cpu->Flag.C;
+
+    Byte result = (original >> 1) | (oldCarry << 7);
+    WriteByte(Cycles, result, memory, addr);
+
+    RORSetStatus(original, result, cpu);
 }
 
-
 /*
-   ROL_ABSX - Load Accumulator from Absolute ROLress with X Offset.
-   Similar to ROL_ABS, but ROLs the X register value to the absolute ROLress before reading
+   ROR_ABSX - Load Accumulator from Absolute RORress with X Offset.
+   Similar to ROR_ABS, but RORs the X register value to the absolute RORress before reading
    the value from memory. It adjusts the cycle count accordingly and sets the status flags.
 */
 
 
-void ROL_ABSX(Word *Cycles, MEM6502 *memory, CPU6502 *cpu) {
-    Word Absolute = FetchWord(Cycles, memory, cpu);
-    Absolute += cpu->X;
+void ROR_ABSX(Word *Cycles, MEM6502 *memory, CPU6502 *cpu) 
+{
+    Word addr = FetchWord(Cycles, memory, cpu);
+    addr += cpu->X;
 
-    Byte Value = ReadByte(Cycles, Absolute, memory);
-    ROLSetStatus(Value, cpu);
-    cpu->A = (Value >> 1) | (cpu->Flag.C << 7);
+    Byte original = ReadByte(Cycles, addr, memory);
+    Byte oldCarry = cpu->Flag.C;
+
+    Byte result = (original >> 1) | (oldCarry << 7);
+    WriteByte(Cycles, result, memory, addr);
+
+    RORSetStatus(original, result, cpu);
 }
 
-#endif // ROL_H
+#endif // ROR_H
