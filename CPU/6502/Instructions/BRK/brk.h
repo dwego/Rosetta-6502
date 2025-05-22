b235f371d70aed3725654efd2b5f72e42a5b93aa
dwego <97767711+dwego@users.noreply.github.com>
2025-05-22 00:48:25 -0300

    Add BRK Instruction

---
+++ CPU/6502/Instructions/BRK/brk.h
@@ -0,0 +1,35 @@
+#ifndef BRK_H
+#define BRK_H
+
+#include "config.h"
+#include "cpu6502.h"
+#include "mem6502.h"
+
+void BRK(Word *Cycles, MEM6502 *mem, CPU6502 *cpu) {
+    // Incrementa PC para o próximo endereço (2 bytes)
+    cpu->PC += 2;
+    (*Cycles)--;  
+
+    // Empilha PC (high byte, depois low byte)
+    PushPCToStack(Cycles, mem, cpu);
+
+    // Prepara status para empilhamento: PS com bit B setado (bit 4)
+    Byte status_with_B = cpu->PS | 0x10;
+
+    // Empilha o status com o bit B setado
+    PushByteToStack(Cycles, mem, status_with_B, cpu);
+
+    // Set Interrupt Disable flag para bloquear interrupções futuras
+    cpu->Flag.I = 1;
+    cpu->PS |= (1 << 2); // bit I setado no PS
+
+    // O bit B NÃO deve ficar setado na CPU, apenas no status empilhado
+
+    // Lê vetor de interrupção (IRQ/BRK) em $FFFE/$FFFF
+    Byte lo = mem->Data[0xFFFE];
+    Byte hi = mem->Data[0xFFFF];
+    cpu->PC = (hi << 8) | lo;
+    (*Cycles) -= 2;
+}
+
+#endif // BRK_H
