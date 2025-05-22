701e198f1a15112cee45b870b9f8daf0e89ea658
dwego <97767711+dwego@users.noreply.github.com>
2025-05-22 17:00:43 -0300

    fix(cpu6502): fix JSR instruction implementation

--- src/cpu/Instructions/JSR/jsr.h
+++ src/cpu/Instructions/JSR/jsr.h
@@ -4,28 +4,29 @@
 #include "config.h"
 #include "cpu6502.h"
 
+/*
+   This is a header file for the JSR (Jump to Subroutine) and RTS (Return from
+   Subroutine) instructions for MOS Technology 6502. JSR is used to jump to a
+   subroutine, saving the return address, and RTS is used to return from that
+   subroutine. For more information about the instructions, refer to
+   Instructions.MD
+*/
 
-/* 
-   This is a header file for the JSR (Jump to Subroutine) and RTS (Return from Subroutine) instructions for MOS Technology 6502.
-   JSR is used to jump to a subroutine, saving the return address, and RTS is used to return from that subroutine.
-   For more information about the instructions, refer to Instructions.MD
-*/ 
-
-
-/* 
+/*
    JSR - Jump to Subroutine:
-   This function fetches a two-byte address from memory, saves the return address (PC + 2) to the stack,
-   and then sets the program counter (PC) to the specified address.
-   It adjusts the cycle count accordingly.
-*/ 
+   This function fetches a two-byte address from memory, saves the return
+   address (PC + 2) to the stack, and then sets the program counter (PC) to the
+   specified address. It adjusts the cycle count accordingly.
+*/
 
-
-static inline void JSR(Word *Cycles, MEM6502 *memory, CPU6502 *cpu) {
-  Word Sub_Addr = FetchWord(Cycles, memory, cpu);
-  PushPCToStack(Cycles, memory, cpu);
-  cpu->PC = Sub_Addr;
-  (*Cycles)--;
-   spend_cycles(6);
+static inline void
+JSR (Word *Cycles, MEM6502 *memory, CPU6502 *cpu)
+{
+  Word SubAddr = FetchWord (Cycles, memory, cpu);
+  Word ReturnAddr = cpu->PC - 1;
+  PushWordToStack (Cycles, memory, ReturnAddr, cpu);
+  cpu->PC = SubAddr;
+  spend_cycles (6);
 }
 
 #endif // JSR_H
