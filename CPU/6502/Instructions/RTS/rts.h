212db10cc1718e996dd314842ab7acfa3c241456
dwego <dweg0epic@outlook.com>
2025-05-22 02:16:48 -0300

    Create RTS/rts.h file

---
+++ CPU/6502/Instructions/RTS/rts.h
@@ -0,0 +1,22 @@
+#ifndef RTS_H
+#define RTS_H
+
+#include "config.h"
+#include "cpu6502.h"
+
+
+/* 
+   RTS - Return from Subroutine:
+   This function retrieves the return address from the stack and sets the program counter (PC) to that address.
+   It adjusts the cycle count accordingly.
+*/ 
+
+
+void RTS(Word *Cycles, MEM6502 *memory, CPU6502 *cpu) {
+  Word ReturnAddress = PopWordFromStack(Cycles, memory, cpu);
+  cpu->PC = ReturnAddress;
+  (*Cycles)-=2;
+}
+
+
+#endif // RTS_H
