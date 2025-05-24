ab06180d7a0b6d1bcbb6735c0797d55e2df4db87
dwego <dweg0epic@outlook.com>
2025-05-24 16:08:07 -0300

    test(rt): implement register transfer instruction unit tests

--- tests/test.c
+++ tests/test.c
@@ -1,4 +1,5 @@
 #include "instructions/ld/test_ld.h"
+#include "instructions/rt/test_rt.h"
 #include "instructions/st/test_st.h"
 #include "test_template.h"
 
@@ -7,9 +8,7 @@ main (void)
 {
   UNITY_BEGIN ();
 
-  test_all_sta ();
-  test_all_stx ();
-  test_all_sty ();
+  test_all_rt ();
 
   return UNITY_END ();
 }
